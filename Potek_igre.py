9eb4876b82eee7be0288ff9bcda83e21ec588540
Nika <nika.kastelec@gmail.com>
2017-04-05 10:01:54 +0200

    Nadaljevali potek igre

--- Potek_igre.py
+++ Potek_igre.py
@@ -1,15 +1,16 @@
 class Polje():
-    def __init__(self, canvas, id_polja, koordinate, sosedi):#master?
+    def __init__(self, canvas, id_polja, koordinate):
         self.canvas = canvas 
         self.id_polja = id_polja
         self.koordinate = koordinate
         self.zasedeno = False
-        self.sosedi = sosedi
+        self.sosedi = []
+        self.namen = 'navadno' #lahko je se vstopno_a, vstopno_b ali zmagovalec_a, zmagovalec_b
 
     def __repr__(self):
-        return ('Ploje id: {}, koordinate: {}, sosedi: {}'.format(self.id_polja, self.koordinate, self.sosedi))
+        return ('Id polja: {}, koordinate: {}, sosedi: {}, namen: {}'.format(self.id_polja, self.koordinate, self.sosedi, self.namen))
 
-    #def ali_je_sosed(self):
+
         
 
 def sredisce(lst):
@@ -17,3 +18,19 @@ def sredisce(lst):
     (x1, y1, x2, y2) = lst
     return ((x1+x2)/2, (y1+y2)/2)
     
+
+
+class Figura():
+    def __init__(self, canvas, id_figure, polozaj, ekipa):
+        self.canvas = canvas
+        self.id_figure = id_figure
+        self.polozaj = polozaj  #id polja, na katerem se nahaja
+        self.ekipa = ekipa #modri in rdeci
+
+        #lisice:
+        self.lisica = PhotoImage('file=lisica.gif')
+        #self.id_lisica1 = self.canvas.create_image()
+
+
+
+
