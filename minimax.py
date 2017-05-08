dc19b08ffcaa965111a452f88fda1edc0915e8bf
Petra Reberc <petrareberc@hotmail.com>
2017-05-08 20:10:11 +0200

    Dopolnjeni komentarji

--- minimax.py
+++ minimax.py
@@ -2,11 +2,8 @@ import logging
 
 from Igra import *
 
-razdalje_zajci = {14: 1, 12: 1, 15: 2, 25: 2, 11: 2, 16: 3, 26: 3, 24: 3, 10: 3, 17: 4,
-                  9: 4}  # razdalja do zmagovalnega polje ki jih gledajo zajci
-razdalje_lisice = {2: 1, 4: 1, 1: 2, 5: 2, 21: 2, 0: 3, 6: 3, 20: 3, 22: 3, 19: 4, 7: 4}
-######################################################################
-## Algoritem minimax
+
+# Algoritem minimax
 
 class Minimax:
     # Algoritem minimax predstavimo z objektom, ki hrani stanje igre in
@@ -23,18 +20,18 @@ class Minimax:
 
 
     def prekini(self):
-        """Metoda, ki jo pokliče GUI, Äe je treba nehati razmišljati, ker
-           je uporabnik zaprl okno ali izbral novo igro."""
+        #Metoda, ki jo pokliče GUI, če je treba nehati razmišljati, ker je uporabnik zaprl okno ali izbral novo igro.
         self.prekinitev = True
 
     def izracunaj_potezo(self, igra):
-        """IzraÄunaj potezo za trenutno stanje dane igre."""
-        # To metodo pokliÄemo iz vzporednega vlakna
+        # Izračuna potezo za trenutno stanje dane igre
+        # To metodo pokliÄčemo iz vzporednega vlakna
         self.igra = igra
-        self.prekinitev = False # Glavno vlakno bo to nastvilo na True, Äe moramo nehati
+        self.prekinitev = False # Glavno vlakno bo to nastvilo na True, Äče moramo nehati
         self.jaz = self.igra.na_potezi
-        self.poteza = None, None # Sem napiĹĄemo potezo, ko jo najdemo
-        # PoĹženemo minimax
+        self.poteza = None, None # Sem napišemo potezo, ko jo najdemo. Poteza je oblike (figura, polje)
+        #  kje je figura oblike (kdo, kje je zdaj). figura[1] je None, če je na začetnih koordinatah
+        # Poženemo minimax
         (figura, polje, vrednost) = self.minimax(self.globina, True)
         self.jaz = None
         self.igra = None
@@ -44,10 +41,10 @@ class Minimax:
             self.poteza = figura, polje
 
     # Vrednosti igre
-    ZMAGA = 100000 # Mora biti vsaj 10^5
+    ZMAGA = 100000
     NESKONCNO = ZMAGA + 1 # Več kot zmaga
 
-    #kje se figure premikajo
+    #kje se figure premikajo (seznam idjev povezanih polj. Zadnje polje v seznamu je vedno zmagovalno)
     POTI_ZA_ZAJCE = [[3,4,5,6,7,8,9,10,11,12,13], [3,2,1,0,19,18,17,16,15,14,13], [3,4,21,22,23,24,25,14,13], [3,4,5,6,22,23,24,10,11,12,13],
                      [3,2,1,0,20,27,26,25,14,13], [3,2,1,0,20,27,26,16,15,14,13], [6,7,8,9,10, 24,25,14,13], [0,19,18,17,16,26,25,14,13]]
     POTI_ZA_LISICE = [[13,12,11,10,9,8,7,6,5,4,3], [13,14,15,16,17,18,19,0,1,2,3], [13,12,11,10,24,23,22,21,4,3], [13,14,15,16,26,27,20,21,4,3],
@@ -141,7 +138,7 @@ class Minimax:
 
 
 
-                    # poskrbimo da bodo nekater figure branile cilj:
+                # poskrbimo da bodo nekatere figure branile cilj:
                 napadalne_lisice = 0
                 for i in self.igra.lisice:
                     if i in [1,2,3,4,5,21,22,20]:
@@ -206,7 +203,6 @@ class Minimax:
 
 
     def minimax(self, globina, maksimiziramo):
-
         if self.prekinitev:
             logging.debug ("Minimax prekinja, globina = {0}".format(globina))
             return (None, None, 0)
